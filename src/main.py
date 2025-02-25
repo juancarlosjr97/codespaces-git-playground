cdef2235e5f7872062dc79fa41335e59852a5414
Aarbaz Alam <39804950+Aarbaz77@users.noreply.github.com>
2025-02-25 12:43:00 +0000

    Update python strftime to have seconds

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 if __name__ == "__main__":
-    current_datetime = datetime.now().strftime("%d-%m-%Y %H:%M")
+    current_datetime = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     message = f"Congratulations, you are running the a python script from a GitHub Codespace environment. Now, it is {current_datetime} and time for further development."
     
     print(message)
